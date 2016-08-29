691c6aacdc8f19091adf519bb86c715f81598cd2
dvdsouza <dvdcmd@gmail.com>
2016-08-29 10:10:42 -0300

    changing a react component state with setState

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,11 +2,29 @@ import Chance from 'chance';
 import React from 'react';
 
 class Detail extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			name: chance.first(),
+			country: chance.country({ full: true })
+		};
+	}
+
+	buttonClicked() {
+		const newState = {
+			name: chance.first()
+		};
+
+		this.setState(newState);
+	}
+
 	render() {
-		return <p>
-		{chance.first() == 'John' 
-						  ? 'Hello, John' 
-						  : 'Hello, Another one'}!</p>;
+		return (<div>
+				<p>Heelo, {this.state.name}.</p>
+				<p>You're from {this.state.country}.</p>
+				<button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+			</div>);
 	}
 }
 
