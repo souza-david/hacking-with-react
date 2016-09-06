aae84a6874b410a53210d45cfc7f01164b456861
dvdsouza <dvdcmd@gmail.com>
2016-09-06 09:30:17 -0300

    routes organizaded hierarchically

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
+import App from './pages/App';
 import Detail from './pages/Detail';
 import List from './pages/List';
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={ List } />
-        <Route path="/react" component={ Detail } />
+    <Router history={browserHistory}>
+        <Route path="/" component={ App }>
+            <IndexRoute component={ List } />
+            <Route path="detail/:repo" component={ Detail } />
+        </Route>
     </Router>,
     document.getElementById('app')
 );
