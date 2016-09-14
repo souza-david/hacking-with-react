2484e39028030b7c82895e8f57f3412d7365445d
David Souza <dvdcmd@gmail.com>
2016-09-14 14:26:52 -0300

    setting application title

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Demo React Application</h1>
+                <h1>Demo React Application v1.0</h1>
                 {this.props.children}
             </div>
         );
